9308fad0294fa4a597fd4ced2ed369e16578c9b8
anacasroline <95311688+anacasroline@users.noreply.github.com>
2024-08-21 11:35:10 -0300

    Add files via upload

--- oi.c
+++ oi.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-int main () {
-    printf("oi, mundo!\n");
-    return 0;
+
+void Maior(int a, int b, int x){
+    if(a>b && a>x ){
+        printf("%d",a);
+    }else if(b>a && b>x) {
+        printf("%d", b);
+    }else if (x>b && x>a) {
+        printf("%d", x);
+    }
 }
 
+int main () {
+    int a;
+    int b;
+    int x;
+    scanf("%d %d %d", &a,&b,&x);
+    Maior(a,b,x);
+    return 0;
+}
